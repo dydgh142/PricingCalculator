2838baed5db7bad46a20f59e48e4a949736b58ce
Cho yoon ho <98027471+dydgh142@users.noreply.github.com>
2025-02-17 13:52:38 +0900

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';  // PricingCalculator 컴포넌트를 여기로 이동
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
